fea070c53f2cde1d04c9ce787e3f99e2ab9cab63
Saitama1669 <144035264+Saitama1669@users.noreply.github.com>
2023-10-10 01:29:06 -0700

    Airsoftartseostudio

---
+++ main.js
@@ -0,0 +1,80 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=>
+{hamburger.classList.remove("active");
+ navMenu.classList.length("active");
+}));
+
+document.onclick = function(e){
+    if(!hamburger.contains(e.target)&&!navMenu.contains(e.target)){
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+}
+
+
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+const textArray = [
+"Web Design",
+"App Innovation",
+"SEO Mastery",
+"Digital Marketing",
+"E-commerce Solutions",
+"Content Creation",
+"Social Media Management",
+"Analytics Insights"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+if (charIndex < textArray[textArrayIndex].length) {
+if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+charIndex++;
+setTimeout(type, typingDelay);
+} 
+else {
+cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+}
+}
+
+function erase() {
+if (charIndex > 0) {
+if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+charIndex--;
+setTimeout(erase, erasingDelay);
+} 
+else {
+cursorSpan.classList.remove("typing");
+textArrayIndex++;
+if(textArrayIndex>=textArray.length) textArrayIndex=0;
+setTimeout(type, typingDelay + 1100);
+}
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+
+
+
+
+
+
+
+
